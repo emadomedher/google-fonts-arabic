f941bcaa88f9b633f63618cbd3d10b205c3ebc9e
emadomedher <lsumedher@gmail.com>
2018-11-22 13:56:24 +0300

    Adding constants

--- lib/google_fonts_arabic.dart
+++ lib/google_fonts_arabic.dart
@@ -1,1 +1,23 @@
 library google_fonts_arabic;
+
+class ArabicFonts {
+
+  final String Amiri = 'Amiri';
+  final String Aref_Ruqaa = 'Aref_Ruqaa';
+  final String Baloo_Bhaijaan = 'Baloo_Bhaijaan';
+  final String Cairo = 'Cairo';
+  final String Changa = 'Changa';
+  final String El_Messiri = 'El_Messiri';
+  final String Harmattan = 'Harmattan';
+  final String Jomhuria = 'Jomhuria';
+  final String Katibeh = 'Katibeh';
+  final String Lalezar = 'Lalezar';
+  final String Lateef = 'Lateef';
+  final String Lemonada = 'Lemonada';
+  final String Mada = 'Mada';
+  final String Markazi_Text = 'Markazi_Text';
+  final String Mirza = 'Mirza';
+  final String Rakkas = 'Rakkas';
+  final String Reem_Kufi = 'Reem_Kufi';
+  final String Scheherazade = 'Scheherazade';
+}
