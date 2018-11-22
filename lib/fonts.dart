54201f368834cc385235543710d83990c9cd24a6
emadomedher <lsumedher@gmail.com>
2018-11-22 14:59:38 +0300

    Adding constants

---
+++ lib/fonts.dart
@@ -0,0 +1,24 @@
+library google_fonts_arabic;
+
+class ArabicFonts {
+  ArabicFonts._();
+  static const String Amiri = 'Amiri';
+  static const String Aref_Ruqaa = 'Aref_Ruqaa';
+  static const String Baloo_Bhaijaan = 'Baloo_Bhaijaan';
+  static const String Cairo = 'Cairo';
+  static const String Changa = 'Changa';
+  static const String El_Messiri = 'El_Messiri';
+  static const String Harmattan = 'Harmattan';
+  static const String Jomhuria = 'Jomhuria';
+  static const String Katibeh = 'Katibeh';
+  static const String Lalezar = 'Lalezar';
+  static const String Lateef = 'Lateef';
+  static const String Lemonada = 'Lemonada';
+  static const String Mada = 'Mada';
+  static const String Markazi_Text = 'Markazi_Text';
+  static const String Mirza = 'Mirza';
+  static const String Rakkas = 'Rakkas';
+  static const String Reem_Kufi = 'Reem_Kufi';
+  static const String Scheherazade = 'Scheherazade';
+  static const String Tajawal = 'Tajawal';
+}
